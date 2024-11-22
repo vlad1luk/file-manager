50f982047cb11656ee5333ee86ccc7c453c7d0ca
vlad1luk <lukyanovvladyslav@gmail.com>
2024-11-21 21:41:47 -0500

    a

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import { useState } from "react";
+import "./App.css";
+
+
+function App() {
+  return (
+    <div>
+      {console.log("salut")}      
+    </div>
+  );
+}
+
+
+export default App;
